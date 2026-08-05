15f5ee00137fc0f5268a77f7bfcab933c56a8fcc
tawtaw200416-commits <tawtaw200416@gmail.com>
2026-08-05 11:32:58 +0630

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -22,9 +22,9 @@ const getSlotResult = (value) => {
 
   // 777 / BAR / အသီးများအတွက် တရားဝင် သင်္ကေတနှင့် ဆုကြေးများ
   const symbols = {
-    0: { name: '🏷️ BAR BAR BAR', reward: 0.00170 },    // BAR = 0.00170 GRAM
-    1: { name: '🍇 🍇 🍇',       reward: 0.00025 },   // Grape = 0.00025 GRAM
-    2: { name: '🍋 🍋 🍋',       reward: 0.00045 },   // Lemon = 0.00045 GRAM
+    0: { name: '🏷️ BAR BAR BAR', reward: 0.00200 },    // BAR = 0.0020 GRAM
+    1: { name: '🍇 🍇 🍇',       reward: 0.00030 },   // Grape = 0.00030 GRAM
+    2: { name: '🍋 🍋 🍋',       reward: 0.00040 },   // Lemon = 0.00040 GRAM
     3: { name: '7️⃣ 7️⃣ 7️⃣ (Jackpot)', reward: 0.00250 } // 777 = 0.00250 GRAM (သို့မဟုတ် လိုချင်သောတန်ဖိုး)
   };
 
